8503b207023974d13db333e31305c701c1978fed
Phạm Quốc Dũng <163796580+Dung20225817@users.noreply.github.com>
2024-09-29 16:44:54 +0700

    ChoosingOption61b.java

--- lab01/ChoosingOption.java
+++ lab01/ChoosingOption.java
@@ -3,7 +3,7 @@ public class ChoosingOption {
     public static void main(String[] args) {
         //int option=JOptionPane.showConfirmDialog(null,"Do you want to change to the first class ticket");
         Object[] options = { "Yes", "No" };
-        int choice = JOptionPane.showOptionDialog(null, 
+        int choice = JOptionPane.showOptionDialog(null,
             "Do you want to change to the first class ticket",  // Message
             "select option",       // Title
             JOptionPane.YES_NO_OPTION,   // Option type
