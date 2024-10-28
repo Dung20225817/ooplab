65bd7b3794b97cf276a888287c516b6f825aca4b
Phạm Quốc Dũng <163796580+Dung20225817@users.noreply.github.com>
2024-10-28 13:28:59 +0700

    Add files via upload

---
+++ Aims.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.Cart;
+import model.DigitalVideoDisc;
+
+/**
+ *
+ * @author User
+ */
+public class Aims {
+
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        
+        
+        System.out.println("Total Cost is: ");
+        System.out.println(anOrder.getTotalCost());
+       
+
+    }
+}
